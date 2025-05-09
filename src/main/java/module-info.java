2d148d76376bb1ae3d40ec3716eba888aa9e829a
Ruzten <steven.sanchez.050208@gmail.com>
2025-05-08 19:22:36 -0500

    Avances

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module co.edu.uniquindio.poo.proyectojavafx {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens co.edu.uniquindio.poo.proyectojavafx to javafx.fxml;
+    opens co.edu.uniquindio.poo.proyectojavafx.viewController to javafx.fxml;
+
     exports co.edu.uniquindio.poo.proyectojavafx;
+
+    exports co.edu.uniquindio.poo.proyectojavafx.viewController to javafx.fxml;
 }
