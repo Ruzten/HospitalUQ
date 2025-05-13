30338f0ee69c21c74facbfed6f2a1d114af574f2
Ruzten <steven.sanchez.050208@gmail.com>
2025-05-13 17:48:05 -0500

    +InterfazAdmin :)

--- src/main/java/co/edu/uniquindio/poo/proyectojavafx/HospitalUQAPP.java
+++ src/main/java/co/edu/uniquindio/poo/proyectojavafx/HospitalUQAPP.java
@@ -9,13 +9,13 @@ import javafx.scene.image.Image;
 public class HospitalUQAPP extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("InicioApp.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("InicioAPP.fxml"));
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/Images/HospitalUQLOGO.png")));
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setWidth(1400);
-        primaryStage.setHeight(787);
+        primaryStage.setHeight(800);
         primaryStage.setResizable(false);
         primaryStage.centerOnScreen();
 
