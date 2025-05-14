865a797b7bd773da6c671ebb3abf7f9525111cff
unknown <Nestor321>
2025-05-13 20:20:02 -0500

    .

--- src/main/java/co/edu/uniquindio/poo/proyectojavafx/Model/HospitalUQ.java
+++ src/main/java/co/edu/uniquindio/poo/proyectojavafx/Model/HospitalUQ.java
@@ -61,19 +61,38 @@ public class HospitalUQ {
 
     // METODOS CREAR OBJETOS
 
-    public boolean crearMedico(String id, Genero genero, String nombres, String apellidos, int edad, String telefono, String correo, String direccion,String contraseña, String matriculaLicencia, boolean certificado, String ubicacion,Estado estado, Especialidad especialidad ) {
+    public boolean crearMedico(String id, Genero genero, String nombres, String apellidos, int edad, String telefono, String correo, String direccion, String contraseña, String matriculaLicencia, boolean certificado, String ubicacion, Estado estado, Especialidad especialidad) {
         boolean flag = false;
-                for (Persona persona : listaMedicos) {
-                    if (persona.getId().equals(id)) {
-                        return flag;
+        for (Persona persona : listaMedicos) {
+            if (persona.getId().equals(id)) {
+                return flag;
 
-                    }
-                }
+            }
+        }
         Medico nuevoMedico = new Medico(id, genero, nombres, apellidos, edad, telefono, correo, direccion, contraseña, matriculaLicencia, certificado, null, ubicacion, estado, especialidad);
         listaMedicos.add(nuevoMedico);
         flag = true;
         return flag;
     }
-    public boolean crearPaciente(String id, Genero genero, String nombres, String apellidos,int edad, String telefono, String correo, String direccion, String contraseña)
+
+    public boolean crearPaciente(String id, Genero genero, String nombres, String apellidos, int edad, String telefono, String correo, String direccion, String contraseña, String fechaNacimiento, TipoSangre rh, List<HistorialMedico> listaHistorialMedico) {
+        boolean flag = false;
+        for (Persona persona : listaPacientes) {
+            if (persona.getId().equals(id)) {
+                return flag;
+            }
+        }
+        Paciente nuevoPaciente = new Paciente(id, genero, nombres, apellidos, edad, telefono, correo, direccion, contraseña, fechaNacimiento, rh);
+
+        if (listaDeHistorialesMedicos != null) {
+            for (HistorialMedico historial : listaDeHistorialesMedicos) {
+                nuevoPaciente.getHistorialMedico().add(historial);
+            }
+        }
+        listaPacientes.add(nuevoPaciente);
+        flag = true;
+        return flag;
+    }
+
 
 }
